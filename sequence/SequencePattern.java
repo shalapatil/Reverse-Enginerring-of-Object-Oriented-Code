75d0dd4824aacb00ae83042785b7fa45557bb481
Shalaka Patil <shalu214patil@gmail.com>
2013-09-15 23:26:55 +0530

    This class decides pattern for sequenece element

---
+++ sequence/SequencePattern.java
@@ -0,0 +1,20 @@
+package sequence;
+import java.awt.*;
+
+
+
+public class SequencePattern {
+
+	public Label className;
+
+	void draw(int x,int y)
+	{
+		className.setBounds(x,y,200,20);
+				
+	}
+	
+	
+
+}
+
+
