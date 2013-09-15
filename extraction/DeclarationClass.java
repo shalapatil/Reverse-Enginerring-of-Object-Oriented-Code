8ecb04d298e5f8d36e38e2e2b485ad1a59284962
Shalaka Patil <shalu214patil@gmail.com>
2013-09-15 23:13:05 +0530

    This class is used to declare variables required for data extraction

---
+++ extraction/DeclarationClass.java
@@ -0,0 +1,25 @@
+package extraction;
+
+public class DeclarationClass {
+
+	 static String class1;
+		 static String class_name;
+		static String datatype;
+		 static String extra;
+		 static String class_end;
+		 static String extend;
+		 static String implement;
+		 static String any;
+		 static String name;
+		 static String is_fun;
+		 static String end_fun;
+		 static String fun_call;
+		 static String block_start;
+		 static String block_end;
+		 static String INPUT;
+		 static String mem_allocation;
+		 static String template;
+		 
+	
+	
+}
