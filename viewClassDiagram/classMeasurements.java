2b2b04316425b635bd26a1665dc184527c5a5807
Shalaka Patil <shalu214patil@gmail.com>
2013-09-15 23:30:19 +0530

    This class defines measurement for class for drawing on panel

---
+++ viewClassDiagram/classMeasurements.java
@@ -0,0 +1,28 @@
+package viewClassDiagram;
+
+public interface classMeasurements {
+
+	public final int classHeight=220;
+	public final int classWidth=200;
+	
+	public final int startClassX=70;
+	public final int startClassY=70;
+	
+	public final int lableHeight=20;
+	public final int labelWidth=200;
+	
+	public final int variableListHeight=100;
+	public final int variableListWidth=200;
+	
+	public final int functionListHeight=100;
+	public final int functionListWidth=200;
+	
+	public final int verticalSpace=35;
+	public final int horizontalSpace=50;
+	
+	public final int smallLineDist=10;
+	
+	
+	
+	
+}
